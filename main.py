82b93ee0031952df0b027d4531b00054c91517b4
Voisa Gigel Marinel <voisa.gigel.marinel@gmail.com>
2020-10-29 14:20:56 +0200

    Initial commit to my first project share to github platform

---
+++ main.py
@@ -0,0 +1,3 @@
+if __name__ == '__main__':
+    print("Hello pycharm!\nWelcome into D local disk!")
+    print("we gone use Git from pycharm to found a connection between gitstud from cunbm utcluj and my pycharm")
